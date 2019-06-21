8f63b569c2b1787cd5f9c23f4e4154709952ccb1
cainster <1caindo@gmail.com>
2019-06-21 16:52:57 -0700

    refactor to controlled element

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 //class based component bc using state
 class SearchBar extends React.Component {
+  state = { term: ''};
+
   //naming convention on_"name of the element"_"event we are watching for"
   // onInputChange(event){
   //   console.log(event.target.value);
@@ -20,8 +22,10 @@ class SearchBar extends React.Component {
             <label>Image Search</label>
             <input type="text"
               // onChange={(this.onInputChange)}
-              onChange={(e) => console.log(e.target.value)}
+              // onChange={(e) => console.log(e.target.value)}
               // onClick={this.onInputClick}
+              value={this.state.term}
+              onChange={(e) => this.setState({ term: e.target.value })}
             />
           </div>
         </form>
