f02b87eba48a39c958b112556c75b29e4493ee10
cainster <1caindo@gmail.com>
2019-06-21 16:39:34 -0700

    added event handlers

---
+++ pics/src/components/SearchBar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+//class based component bc using state
+class SearchBar extends React.Component {
+  onInputChange(event){
+    console.log(event.target.value);
+
+  }
+
+  // onInputClick(){
+  //     console.log('input was clicked')
+  // }
+
+  render(){
+    return (
+      <div className="ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Image Search</label>
+            <input type="text"
+              onChange={this.onInputChange}
+              // onClick={this.onInputClick}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
