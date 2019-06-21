732b753a98ed5d093265ff5372581fb8cbaf45ed
cainster <playdough@Cains-MacBook-Pro.local>
2019-06-20 17:12:15 -0700

    added conditional render comp

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -7,7 +7,7 @@ class App extends React.Component {
     //req
     super(props);
     //this is the only time we do direct assignment to this.state
-    this.state = { lat: null };
+    this.state = { lat: null, errorMessage: "" };
 
     window.navigator.geolocation.getCurrentPosition(
       (
@@ -17,14 +17,23 @@ class App extends React.Component {
         this.setState({ lat: position.coords.latitude });
       },
       //failure callback
-      err => console.log(err)
+      err => {
+        this.setState({ errorMessage: err.message });
+      }
     );
   }
 
   //requirement by React
   //runs frequently
   render() {
-    return <div>Latitude: {this.state.lat}</div>;
+    //conditionally render
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div> Error: {this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>latitude: {this.state.lat}</div>;
+    }
+    return <div>Loading!</div>;
   }
 }
 
