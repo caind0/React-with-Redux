3ce4a73aa937641ec2b6e86e14a191ceeba5def3
cainster <playdough@Cains-MacBook-Pro.local>
2019-06-20 19:26:46 -0700

    added icons

--- seasons/src/SeasonsDisplay.js
+++ seasons/src/SeasonsDisplay.js
@@ -10,8 +10,16 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
+  const text = season === 'winter' ? 'Burr, it is chilly' : 'Let\'s hit the beach';
+  const icon = season === 'winter' ? 'snowflake' : 'sun';
 
-  return <div>{season === 'winter' ? 'Burr, it is chilly' : 'Let\'s hit the beach' }</div>;
-};
+  return (
+    <div>
+      <i className={`${icon} icon`} />
+      <h1>{text}</h1>
+      <i className={`${icon} icon`} />
+    </div>
+  );
+}
 
 export default SeasonDisplay;
