9eb1c0bceabedad54dacac3180f57266981a7562
cainster <1caindo@gmail.com>
2019-06-21 18:36:46 -0700

    solved context with this

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -14,10 +14,16 @@ class SearchBar extends React.Component {
   //     console.log('input was clicked')
   // }
 
+  //prevent user from entering enter and reloading page
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state.term);
+  }
+
   render(){
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Image Search</label>
             <input type="text"
