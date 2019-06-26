9b5eeb4268e70ba5883616479c0c6b30eda9fd7d
cainster <1caindo@gmail.com>
2019-06-25 18:34:14 -0700

    finished tiling css

--- pics/src/components/ImageCard.js
+++ pics/src/components/ImageCard.js
@@ -1,14 +1,40 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+  //react refs
+  constructor(props){
+    super(props);
+
+    this.state = { spans:0 };
+
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount(){
+    //callback bound
+    this.imageRef.current.addEventListener('load', this.setSpans);
+
+
+    //console.log(this.imageRef.current.clientHeight);
+  }
+
+  setSpans = () => {
+    //console.log(this.imageRef.current.clientHeight);
+    const height = this.imageRef.current.clientHeight;
+    const spans = Math.ceil(height / 10 );
+
+    this.setState({ spans });
+  }
+
   render(){
     //destructure this this.props
     const { description, urls } = this.props.image
     return (
-      <div>
+      <div style={{gridRowEnd: `span ${this.state.spans}`}}>
         <img
-          alt={description}
-          src={urls.regular}
+          // alt={description}
+          // src={urls.regular}
+          ref={this.imageRef} alt={description} src={urls.regular}
         />
       </div>
     );
