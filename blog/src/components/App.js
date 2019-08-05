605e6c86b4b2e7b1238b332751c05c3fa6ae5f10
cainster <1caindo@gmail.com>
2019-08-05 01:46:53 -0700

    simple blog using redux

---
+++ blog/src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PostList from './PostList';
+
+const App = () => {
+  return (
+    <div className="ui container">Post List</div>
+  );
+};
+
+export default App;
---
+++ blog/src/components/PostList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+//class based componnet
+class PostList extends React.Component{
+  render(){
+    return (
+      <div>Post List</div>
+    );
+  }
+}
+
+export default PostList;
