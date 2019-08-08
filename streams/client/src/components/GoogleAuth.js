2ea93df8c55b91b8fe6834ac2ee52f6d292e9b89
cainster <1caindo@gmail.com>
2019-08-07 20:32:51 -0700

    recording users id

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -25,7 +25,7 @@ class GoogleAuth extends React.Component {
   //callback for status sign in
   onAuthChange = (isSignedIn) => {
     if(isSignedIn){
-      this.props.signIn();
+      this.props.signIn(this.auth.currentUser.get().getId());
     }else{
       this.props.signOut();
     }
