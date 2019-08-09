832950b99bb73180da17cdf79066b3dd336ad552
cainster <1caindo@gmail.com>
2019-08-08 18:14:34 -0700

    displaying form error validation

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -5,13 +5,27 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
+  renderError({error,touched}){
+    if(touched && error){
+      return(
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  }
+
   //remember to destructure props
-  renderInput({input, label }){
+  renderInput = ({input, label, meta }) =>{
     // console.log(formProps)
+    //show error
+    // console.log(meta.error);
+
     return (
       <div className="field">
         <label>{label}</label>
-        <input {...input} />
+        <input {...input} autoComplete='off'/>
+        {this.renderError(meta)}
       </div>
     );
   }
@@ -24,7 +38,8 @@ class StreamCreate extends React.Component {
     // console.log(this.props);
 
     return (
-      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
         <Field name='title' component={this.renderInput} label="Enter Title" />
         <Field name='description' component={this.renderInput} label="Enter Description" />
         <button className="ui button primary">Submit</button>
@@ -33,6 +48,20 @@ class StreamCreate extends React.Component {
   }
 }
 
+//validate form
+const validate = (formValues) => {
+  const errors = {};
+  if(!formValues.title){
+    //only run if user did not enter a Title
+    errors.title = 'You must enter a title';
+  }
+  if(!formValues.description){
+    errors.description = "You must enter a description";
+  }
+  return errors;
+}
+
 export default reduxForm({
-  form: 'STREAM_CREATE'
+  form: 'STREAM_CREATE',
+  validate: validate
 })(StreamCreate);
