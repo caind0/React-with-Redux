0453db68c7199b3a304db9d9855cc56f586193ef
cainster <1caindo@gmail.com>
2019-08-08 17:41:57 -0700

    handling form

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -1,7 +1,31 @@
 import React from 'react';
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
-};
+//reduxForm is a func that has same functionality as connect function from react redux library, make sure
+//we can call some action creator and get some form data into our component.
+import { Field, reduxForm } from 'redux-form';
 
-export default StreamCreate;
+class StreamCreate extends React.Component {
+  //remember to destructure props
+  renderInput({input, label }){
+    // console.log(formProps)
+    return (
+      <div className="field">
+        <label>{label}</label>
+        <input {...input} />
+      </div>
+    );
+  }
+
+  render(){
+    return (
+      <form className="ui form">
+        <Field name='title' component={this.renderInput} label="Enter Title" />
+        <Field name='description' component={this.renderInput} label="Enter Description" />
+      </form>
+    );
+  }
+}
+
+export default reduxForm({
+  form: 'STREAM_CREATE'
+})(StreamCreate);
