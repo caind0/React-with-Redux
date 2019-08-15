450d240b7ff70c05f5777c4e8e19243081b6acc2
cainster <1caindo@gmail.com>
2019-08-15 15:56:34 -0700

    fetching all streams with class comp

--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import {fetchStreams} from '../../actions';
 
-const StreamList = () => {
-  return <div>StreamList</div>;
-};
+// const StreamList = () => {
+//   return <div>StreamList</div>;
+// };
+//use class bassed comp bc we to call our action creator inside compdidmount ( attempt fetch stream one tiome)
+class StreamList extends React.Component {
+  componentDidMount(){
+    this.props.fetchStreams();
+  }
+  render(){
+    return <div>StreamList</div>;
+  }
+}
 
-export default StreamList;
+export default connect(null, { fetchStreams })(StreamList);
--- streams/client/src/reducers/streamReducer.js
+++ streams/client/src/reducers/streamReducer.js
@@ -3,12 +3,14 @@ import {
   CREATE_STREAM,
   FETCH_STREAM,
   FETCH_STREAMS,
-  EDIT_STREAMS,
-  DELETE_STREAMS
+  EDIT_STREAM,
+  DELETE_STREAM
 } from '../actions/types';
 
 export default (state = {}, action) => {
   switch (action.type){
+    case FETCH_STREAMS:
+      return { ...state, ..._.mapKeys(action.payload,'id') };
     case FETCH_STREAM:
       return { ...state, [action.payload.id]: action.payload };
     case CREATE_STREAM:
@@ -16,7 +18,8 @@ export default (state = {}, action) => {
     case EDIT_STREAM:
       return { ...state, [action.payload.id]: action.payload };
     case DELETE_STREAM:
-
+      //no id bc when we dispatch in actions/index.js we have the id in the payload already
+      return _.omit(...state, action.payload)
     default:
       return state;
   }
