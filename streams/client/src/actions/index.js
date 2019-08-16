63fdaf9ff3d602a3f0091c62d5281ef364a39500
cainster <1caindo@gmail.com>
2019-08-15 17:27:18 -0700

    created browser history object and router

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -26,6 +26,7 @@ export const createStream = (formValues) => async (dispatch, getState) => {
   const response = await streams.post('/streams',{ ...formValues,userId });
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
+  //do some programatic navigation to get user back to root route
 };
 
 export const fetchStreams = () => async dispatch => {
