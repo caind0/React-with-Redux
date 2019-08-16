6f62800a4d0ca7232ecf4f53505a286b19ed0864
cainster <1caindo@gmail.com>
2019-08-16 03:25:04 -0700

    loading initial values in stream edit

--- streams/client/src/components/streams/StreamEdit.js
+++ streams/client/src/components/streams/StreamEdit.js
@@ -1,18 +1,32 @@
+import _ from 'lodash';
 import React from 'react';
+
 import { connect } from 'react-redux';
-import { fetchStream } from '../../actions';
+import { fetchStream, editStream } from '../../actions';
+import StreamForm from './StreamForm';
 
 //refactor to class based component
 class StreamEdit extends React.Component {
   componentDidMount(){
     this.props.fetchStream(this.props.match.params.id);
   }
+
+  onSubmit = (formValues) => {
+    console.log(formValues);
+  };
+
   render(){
     if(!this.props.stream){
       return <div>Loading...</div>
     }
     return(
-      <div>{this.props.stream.title}</div>
+      <div>
+        <h3>Edit a Stream</h3>
+        <StreamForm
+          initialValues={_.pick(this.props.stream, 'title','description')}
+          onSubmit={this.onSubmit}
+        />
+      </div>
     );
   }
 }
@@ -26,4 +40,4 @@ const mapStateToProps = (state,ownProps) => {
   return { stream: state.streams[ownProps.match.params.id]};
 }
 
-export default connect (mapStateToProps,{fetchStream})(StreamEdit);
+export default connect (mapStateToProps,{fetchStream,editStream})(StreamEdit);
