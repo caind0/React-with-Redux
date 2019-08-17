cbb257e6c8d612271aa6cd268e8824f3d2980dde
cainster <1caindo@gmail.com>
2019-08-16 17:41:25 -0700

    made modal reusable

--- streams/client/src/components/Modal.js
+++ streams/client/src/components/Modal.js
@@ -9,13 +9,12 @@ const Modal = props => {
       className="ui dimmer modals visible active"
       >
       <div onClick={(event) => event.stopPropagation()} className="ui standard modal visible active">
-        <div className="header">Delete Stream</div>
+        <div className="header">{props.title}</div>
         <div className="content">
-          Are you sure you want to delete this stream?
+          {props.content}
         </div>
         <div className="actions">
-          <button className="ui primary button">Delete</button>
-          <button className="ui button">Cancel</button>
+          {props.actions}
         </div>
       </div>
     </div>,
