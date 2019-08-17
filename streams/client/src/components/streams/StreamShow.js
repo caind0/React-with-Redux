bdd983a726312d30af8d1de2a769defdc98921fc
cainster <1caindo@gmail.com>
2019-08-16 21:43:21 -0700

    hooked up streaming, stream using rtmp://localhost:8000 key=id

--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -9,15 +9,37 @@ import flv from 'flv.js';
 //   );
 // };
 class StreamShow extends React.Component {
-constructor(props){
-  super(props);
+  constructor(props) {
+    super(props);
 
-  this.videoRef = React.createRef();
-}
+    this.videoRef = React.createRef();
+  }
 
   componentDidMount(){
-    this.props.fetchStream(this.props.match.params.id);
+    const { id } = this.props.match.params;
+
+    this.props.fetchStream(id);
+    this.buildPlayer();
   }
+
+  componentDidUpdate(){
+    this.buildPlayer();
+  }
+  
+  buildPlayer(){
+    const { id } = this.props.match.params
+
+    if(this.player || !this.props.stream){
+      return;
+    }
+    this.player = flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${id}.flv`
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
+  }
+
   render(){
     if(!this.props.stream){
       return <div>Loading...</div>
