6915e2aaa0d841e4045b6a54121b6f4ad3173dac
cainster <1caindo@gmail.com>
2019-08-16 18:07:08 -0700

    fetching streams for delete

--- streams/client/src/components/streams/StreamDelete.js
+++ streams/client/src/components/streams/StreamDelete.js
@@ -1,27 +1,37 @@
 import React from 'react';
 import Modal from '../Modal';
 import history from '../../history';
+import {connect} from 'react-redux';
+import {fetchStream} from '../../actions';
 
-const StreamDelete = () => {
+class StreamDelete extends React.Component {
+  componentDidMount(){
+    this.props.fetchStream(this.props.match.params.id);
+  }
 
-  const actions = (
-    <React.Fragment>
-      <button className="ui button negative">Delete</button>
-      <button className="ui button">Cancel</button>
-    </React.Fragment>
-  )
+  renderActions(){
+    return (
+     <React.Fragment>
+       <button className="ui button negative">Delete</button>
+       <button className="ui button">Cancel</button>
+     </React.Fragment>
+   );
+  }
 
+render(){
   return (
     <div>
         StreamDelete
         <Modal
           title="Delete Stream"
           content="Are you sure you want to delete this stream?"
-          actions={actions}
+          actions={this.renderActions()}
           onDismiss={()=> history.push('/')}
         />
     </div>
   );
-};
+  }
 
-export default StreamDelete;
+}
+
+export default connect(null,{fetchStream})(StreamDelete);
