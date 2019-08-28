d6bc534fa284474434870670987386c3965e4987
cainster <1caindo@gmail.com>
2019-08-27 23:33:37 -0700

    connecting selector to state

--- translate/src/components/LanguageSelector.js
+++ translate/src/components/LanguageSelector.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import LanguageContext from '../context/LanguageContext';
 
 class LanguageSelector extends React.Component {
-  
+
+  static contextType = LanguageContext;
 
   render(){
+    console.log(this.context);
+
     return(
       <div>
         Select a Language:
-        <i className="flag us" onClick={()=>this.props.onLanguageChange('english')}/>
-        <i className="flag nl" onClick={()=>this.props.onLanguageChange('dutch')}/>
+        <i className="flag us" onClick={()=>this.context.onLanguageChange('english')}/>
+        <i className="flag nl" onClick={()=>this.context.onLanguageChange('dutch')}/>
       </div>
     );
   }
