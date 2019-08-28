61c860090cc53ad0b6cc636cf7cb3fc516fd6ed9
cainster <1caindo@gmail.com>
2019-08-27 18:08:19 -0700

    use react consumer instead of this.context

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -2,14 +2,20 @@ import React from 'react';
 import LanguageContext from '../context/LanguageContext';
 
 class Button extends React.Component{
-
-  static contextType = LanguageContext;
+  renderSubmit(value){
+    return value === 'english' ? 'Submit' : 'Voorleggen'
+  }
+  // static contextType = LanguageContext;
 
   render(){
-    const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
+    // const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
     return (
       <div>
-        <button className="ui primary">{text}</button>
+        <button className="ui primary">
+          <LanguageContext.Consumer>
+            {(value) => this.renderSubmit(value)}
+          </LanguageContext.Consumer>
+        </button>
       </div>
     );
   }
