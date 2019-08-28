d40f2348b8219aa3416b499f067911d1d845dd15
cainster <1caindo@gmail.com>
2019-08-27 17:26:25 -0700

    createContext

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import LanguageContext from '../context/LanguageContext';
 
 class Button extends React.Component{
+
+  static contextType = LanguageContext;
+
   render(){
+    const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
     return (
       <div>
-        <button className="ui primary">Submit</button>
+        <button className="ui primary">{text}</button>
       </div>
     );
   }
