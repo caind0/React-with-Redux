bef86bbbed95218a407874dca86b23463f697160
cainster <1caindo@gmail.com>
2019-08-27 18:16:29 -0700

    pulling data from mult context provideders

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import UserCreate from './UserCreate';
 import LanguageContext from '../context/LanguageContext';
-
+import ColorContext from '../context/ColorContext';
 
 class App extends React.Component{
 
@@ -21,9 +21,11 @@ class App extends React.Component{
           <i className="flag us" onClick={()=>this.handleChange('english')}/>
           <i className="flag nl" onClick={()=>this.handleChange('dutch')}/>
         </div>
-        <LanguageContext.Provider value={this.state.language}>
-        <UserCreate />
-        </LanguageContext.Provider>
+        <ColorContext.Provider value="red">
+          <LanguageContext.Provider value={this.state.language}>
+          <UserCreate />
+          </LanguageContext.Provider>
+        </ColorContext.Provider>
       </div>
     );
   }
--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import LanguageContext from '../context/LanguageContext';
+import ColorContext from '../context/ColorContext';
 
 class Button extends React.Component{
   renderSubmit(value){
@@ -11,11 +12,15 @@ class Button extends React.Component{
     // const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
     return (
       <div>
-        <button className="ui primary">
-          <LanguageContext.Consumer>
-            {(value) => this.renderSubmit(value)}
-          </LanguageContext.Consumer>
-        </button>
+        <ColorContext.Consumer>
+          { (color)=>
+            <button className={`ui button ${color}`}>
+              <LanguageContext.Consumer>
+                {(value) => this.renderSubmit(value)}
+              </LanguageContext.Consumer>
+            </button>
+          }
+        </ColorContext.Consumer>
       </div>
     );
   }
