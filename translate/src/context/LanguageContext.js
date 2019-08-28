7594f74367616f8da48abc63ce399bd9e8ed7951
cainster <1caindo@gmail.com>
2019-08-27 23:23:57 -0700

    created LanguageStore component to seperate business and view logic

--- translate/src/context/LanguageContext.js
+++ translate/src/context/LanguageContext.js
@@ -1,3 +1,25 @@
 import React from 'react'
 
-export default React.createContext('english');
+const Context = React.createContext('english');
+
+export class LanguageStore extends React.Component{
+  state = { language: 'english' };
+
+  onLanguageChange = (language) => {
+    this.setState({ language: language });
+  }
+
+  render(){
+    return(
+      <Context.Provider value={ {...this.state, onLanguageChange} }>
+        {this.props.children}
+      </Context.Provider>
+    );
+  }
+}
+
+export default Context;
+
+//we can now import 2 things from this file
+// import LanguageContext
+// import { LanguageStore }
