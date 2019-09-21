39cbcc001d65d48197d19653ccde168433c4796d
cainster <1caindo@gmail.com>
2019-09-21 01:59:47 -0700

    created user models and validation

--- phonebook/server.js
+++ phonebook/server.js
@@ -6,6 +6,10 @@ const app = express();
 //Connect Database
 connectDB();
 
+//Init Middleware
+app.use(express.json({extended: false}));
+
+
 app.get('/',(req,res)=> res.send({msg: "Welcome to the phonebook api"}));
 
 //Define Routes
