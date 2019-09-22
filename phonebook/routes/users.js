d944f3374082efe02046108c7ffbf6961f6afa78
cainster <1caindo@gmail.com>
2019-09-21 17:02:33 -0700

    implemented salt hashed passwords for user

--- phonebook/routes/users.js
+++ phonebook/routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
-
+const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 
 
@@ -14,13 +14,41 @@ router.post('/', [
     .isEmpty(),
     check('email','Please include a valid email').isEmail(),
     check('password',"Please enter a password with 6 or more characters").isLength({min:6})
-],(req,res)=> {
+], async (req,res)=> {
     //validate
     const errors = validationResult(req);
     if(!errors.isEmpty()){
         return res.status(400).json({errors:errors.array() })
     }
-    res.send('passed');
+
+    const { name, email, password } = req.body
+
+    //check to see if there is user with the email in DB, 
+    try{
+        let user= await User.findOne({email:email});
+        if(user){
+            res.status(400).json({ msg: 'User already exist'});
+        }
+        //create new isntance of user
+        user = new User({
+            name,
+            email,
+            password
+        });
+
+        //encrypt password
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(password,salt);
+        //save pswd to user;
+        await user.save();
+
+        res.send('User Saved');
+
+    } catch(err){
+        console.log(err.message);
+        res.status(500).send('Server Errors')
+    }
 });
 
 module.exports = router;
